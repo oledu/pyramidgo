17ab2aba540d03fecd09d8b9317bc4e9db4d2aa4
oledu <petrer801014@gmail.com>
2025-03-10 22:07:18 +0800

    remove functions, change server loaction to tw

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -1,55 +1,8 @@
-export const fetchParticipants = async () => {
-  try {
-    const response = await fetch(
-      'https://us-central1-pyramidgo-1923c.cloudfunctions.net/getParticipants'
-    );
-    return await response.json();
-  } catch (error) {
-    console.error('Error fetching user data:', error);
-    throw error;
-  }
-};
-
-export const fetchScoringSp = async () => {
-  try {
-    const response = await fetch(
-      'https://us-central1-pyramidgo-1923c.cloudfunctions.net/getScoringSp'
-    );
-    return await response.json();
-  } catch (error) {
-    console.error('Error fetching user data:', error);
-    throw error;
-  }
-};
-
-export const fetchScoringBld = async () => {
-  try {
-    const response = await fetch(
-      'https://us-central1-pyramidgo-1923c.cloudfunctions.net/getScoringBld'
-    );
-    return await response.json();
-  } catch (error) {
-    console.error('Error fetching user data:', error);
-    throw error;
-  }
-};
-
-export const fetchClimbRecords = async () => {
-  try {
-    const response = await fetch(
-      'https://us-central1-pyramidgo-1923c.cloudfunctions.net/getClimbRecords'
-    );
-    return await response.json();
-  } catch (error) {
-    console.error('Error fetching user data:', error);
-    throw error;
-  }
-};
-
 export const fetchData = async () => {
   try {
     const response = await fetch(
-      'https://us-central1-pyramidgo-1923c.cloudfunctions.net/getData'
+      // 'https://us-central1-pyramidgo-1923c.cloudfunctions.net/getData'
+      ' https://asia-east1-pyramidgo-1923c.cloudfunctions.net/getDataTw'
     );
     return await response.json();
   } catch (error) {

91b8d54a7fc9a486ff843cb20f79ec1d5a6fafb6
xujinbin <xujinbin@tdcc.com.tw>
2025-03-17 10:12:47 +0800

    fix ga

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // 這行很重要！
-}
+  output: 'export',  // 這行確保 Next.js 生成靜態站點（Cloudflare Pages 需要）
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // 這會影響所有頁面
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: 'script-src \'self\' \'unsafe-inline\' \'unsafe-eval\' https://www.googletagmanager.com https://www.google-analytics.com;',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'geolocation=(), microphone=(), camera=(), payment=()',
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig 
+
+module.exports = nextConfig;

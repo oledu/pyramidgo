0d2176354e90334e87ded4f7c34463b674c95fa3
xujinbin <xujinbin@tdcc.com.tw>
2025-03-24 17:38:30 +0800

    seo

--- src/app/components/StructuredData.js
+++ src/app/components/StructuredData.js
@@ -3,12 +3,12 @@ export default function StructuredData() {
     '@context': 'https://schema.org',
     '@type': 'SportsEvent',
     name: '抱石猛獸爭霸賽',
-    description: '台灣頂級攀岩比賽，展示最佳攀岩選手的技巧和實力。',
+    description: '台灣攀岩趣味或棟，展示最佳攀岩選手的技巧和實力',
     startDate: '2025-03-02',
-    endDate: '2023-03-29',
+    endDate: '2025-03-29',
     location: {
       '@type': 'Place',
-      name: '全台各地 線下玩耍 線上集合',
+      name: '全台各地 線下挑戰 線上集合',
       address: {
         '@type': 'PostalAddress',
         addressLocality: 'TW',

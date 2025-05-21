862e5812a568d4b90220747672ab6ffa1b8b9e69
oledu <petrer801014@gmail.com>
2025-05-21 22:16:52 +0800

    tofixed

--- src/app/components/Castle.js
+++ src/app/components/Castle.js
@@ -964,7 +964,7 @@ const Castle = ({ data, period, scoresNoLimitsGymDate }) => {
                             </span>
                           </div>
                           <span className="font-bold text-red-400">
-                            {damage} 🩸
+                            {parseFloat(damage.toFixed(1))} 🩸
                           </span>
                         </div>
                       ))}

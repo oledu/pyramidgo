f5a03d77c8541273e8c631cdbd44b037dfb26fd1
oledu <petrer801014@gmail.com>
2025-05-21 22:21:05 +0800

    teampompommax9

--- src/app/utils/calculatePompomTeam.js
+++ src/app/utils/calculatePompomTeam.js
@@ -4,15 +4,18 @@ export function calculatePompomTeam(data) {
     const team = climber.TEAM_NM || 'N/A'; // 確保有隊名
     // 排除以"單人"開頭的隊伍
     if (team.startsWith('單人')) return map;
-    
+
     if (!map[team]) {
       map[team] = {
         TEAM_NM: team,
         TOTAL_POMPOM: 0,
       };
     }
-    // 累加 POMPOM_TOTAL
-    map[team].TOTAL_POMPOM += climber.POMPOM_TOTAL;
+    // 累加 POMPOM_TOTAL，但確保不超過上限 9
+    map[team].TOTAL_POMPOM = Math.min(
+      map[team].TOTAL_POMPOM + climber.POMPOM_TOTAL,
+      9
+    );
     return map;
   }, {});
 

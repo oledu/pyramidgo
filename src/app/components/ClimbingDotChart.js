eb9f4bab0572b1c43b7229486c92b6940195addc
oledu <petrer801014@gmail.com>
2025-06-15 19:19:36 +0800

    date

--- src/app/components/ClimbingDotChart.js
+++ src/app/components/ClimbingDotChart.js
@@ -71,7 +71,7 @@ const ClimbingDotChart = ({ data, period }) => {
         case '202504T':
           return d3.range(11, 30).map((d) => `5/${d}`); // 5/11-5/29
         case '202505T':
-          return d3.range(15, 30).map((d) => `6/${d}`); // 6/15-6/29
+          return d3.range(15, 29).map((d) => `6/${d}`); // 6/15-6/28
         default:
           return d3.range(16, 30).map((d) => `3/${d}`); // 預設值
       }
